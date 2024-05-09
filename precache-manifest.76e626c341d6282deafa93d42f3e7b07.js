dcc6fdb4e638842b230b1de62e742e363e2fe375
Abbev <addev@tuta.io>
2024-05-09 20:34:07 +1000

    Updates

---
+++ precache-manifest.76e626c341d6282deafa93d42f3e7b07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7eed25ccbb62727835f0858f5052465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c541d58979e6a66a24c",
+    "url": "/static/css/main.be95438f.chunk.css"
+  },
+  {
+    "revision": "eeaa967f03c90cb48a2c",
+    "url": "/static/js/2.2cba4cd7.chunk.js"
+  },
+  {
+    "revision": "4c541d58979e6a66a24c",
+    "url": "/static/js/main.fa205999.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7584513cf9dd21654010cf6453ad6476",
+    "url": "/static/media/CreatoDisplay-ExtraBold.ttf.7584513c.woff"
+  },
+  {
+    "revision": "0644ffd8cc31b1e53f8a3110127d3119",
+    "url": "/static/media/logo.0644ffd8.svg"
+  }
+]);
