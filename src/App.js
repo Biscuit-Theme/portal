445f874df27bb8430e70a16213c9f801edf873f4
Abbev <addev@tuta.io>
2024-05-09 19:53:16 +1000

    Init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div className="biscuit-portal">
+      <div className="glossy-box">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>Biscuit.</h1>
+        <button className="button">Click Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
